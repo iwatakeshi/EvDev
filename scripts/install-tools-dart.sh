90bedee527e72c73c2d57bfd871608bd01835d06
Takeshi Iwana <iwatakeshi@gmail.com>
2019-04-14 16:07:15 -0600

    Attempt to fix build issues

--- scripts/install-tools-dart.sh
+++ scripts/install-tools-dart.sh
@@ -1,5 +1,4 @@
 apt-get update
-apt-get install apt-transport-https
 sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
 sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
 apt-get update
--- scripts/install-tools-dotnet.sh
+++ scripts/install-tools-dotnet.sh
@@ -2,5 +2,4 @@
 
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
-apt-get install -y apt-transport-https
 apt-get update && apt-get install -y dotnet-sdk-2.2 
